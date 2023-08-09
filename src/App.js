6ad6fb5defbe9d2d9e0e92d510716a1c03eac0e3
Kishan M A <kishanmirajkar27@gmail.com>
2023-08-09 22:12:35 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          second commit
+          third commit
         </p>
         <a
           className="App-link"
