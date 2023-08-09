dc2366c509e46593b6c2ec1cf2e953535256ec13
Kishan M A <kishanmirajkar27@gmail.com>
2023-08-09 22:04:26 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          First commit
+          second commit
         </p>
         <a
           className="App-link"
